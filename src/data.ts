fcef17b7b252ac6cf3446349b5b40d8ee15f1559
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-11-30 21:05:05 -0300

    Feat: finalizando aula 02

---
+++ src/data.ts
@@ -0,0 +1,20 @@
+import { character, GENDER, movie } from "./types";
+
+
+export const movies: movie[] = [
+    {
+        id: 1,
+        title: "X-men: O filme",
+        year: 2000
+    }
+]
+
+
+export const characters:character[] =[
+    {
+        id: 1,
+        name: "Storm",
+        gender:GENDER.FEMALE,
+        description: "Melhor dos melhores"
+    }
+]
