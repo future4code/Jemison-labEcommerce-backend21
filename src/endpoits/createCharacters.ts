84d89e38857fca2b20c32dd60afce097bc8eddbd
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-12-03 16:19:59 -0300

    Feat: finalizando a aula 5

--- src/endpoits/createCharacters.ts
+++ src/endpoits/createCharacters.ts
@@ -1,5 +1,5 @@
 import { Request, Response} from "express"
-import { characters } from "../data"
+
 
 export function createCharacter(
     req:Request,
@@ -8,12 +8,12 @@ export function createCharacter(
 
     const {name, gender, description} = req.body
 
-    characters.push({
-        id: Date.now(),
-        name,
-        gender,
-        description
-    })
+    // characters.push({
+    //     id: Date.now(),
+    //     name,
+    //     gender,
+    //     description
+    // })
 
 
     resp.status(201).end()
--- src/endpoits/deleteCharacter.ts
+++ src/endpoits/deleteCharacter.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { characters } from "../data";
+
 
 export function deleteCharacter (
     req:Request, 
@@ -7,13 +7,13 @@ export function deleteCharacter (
     
         const {id} = req.params
         
-        const index:number = characters.findIndex(
-            character => character.id === Number(id)
-        )
+        // const index:number = characters.findIndex(
+        //     character => character.id === Number(id)
+        // )
         
-        if( index > -1){
-            characters.splice(index, 1)
-        }
+        // if( index > -1){
+        //     characters.splice(index, 1)
+        // }
         
         resp.status(200).end()
 }
